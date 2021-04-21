0b004c51d075ac7937e8cbf15f13ae17de656b90
Gezahegn Starr <starrgez@msu.edu>
2021-04-21 16:13:10 -0400

    fixed auth for users

--- client/routes.js
+++ client/routes.js
@@ -1,13 +1,13 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { Login, Signup } from './components/AuthForm';
-import Home from './components/Home';
-import {me} from './store'
-import SingleProduct from './components/SingleProduct';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Login, Signup } from "./components/AuthForm";
+import Home from "./components/Home";
+import { me } from "./store";
+import SingleProduct from "./components/SingleProduct";
 
 // import {AccountInfo} from './components/AccountInfo';
-import { Cart } from './components/Cart';
+import { Cart } from "./components/Cart";
 
 /**
  * COMPONENT
@@ -49,16 +49,16 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
     // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
 
-    isLoggedIn: true //!!state.auth.id
-  }
-}
+    isLoggedIn: !!state.auth.id,
+  };
+};
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
     loadInitialData() {
       dispatch(me());
--- script/seed.js
+++ script/seed.js
@@ -1,7 +1,7 @@
-'use strict'
+"use strict";
 
-const {db,User } = require('../server/db')
-const Product = require('../server/db/models/product')
+const { db, User } = require("../server/db");
+const Product = require("../server/db/models/product");
 
 /**
  * seed - this function clears the database, updates tables to
@@ -10,79 +10,93 @@ const Product = require('../server/db/models/product')
 
 const products = [
   {
-    title: 'Top Hat',
-    description: 'An elegant top hat for formal evening wear',
+    title: "Top Hat",
+    description: "An elegant top hat for formal evening wear",
     price: 10000,
-    pictureURL: 'https://cdn.shopify.com/s/files/1/0939/7260/products/Top_Hat00526_2048x.jpg?v=1527557961',
+    pictureURL:
+      "https://cdn.shopify.com/s/files/1/0939/7260/products/Top_Hat00526_2048x.jpg?v=1527557961",
     rating: 0,
     quantity: 250,
   },
   {
-    title: 'Baseball Cap',
-    description: 'A stylish and comfortable way to represent you team!',
+    title: "Baseball Cap",
+    description: "A stylish and comfortable way to represent you team!",
     price: 2500,
-    pictureURL: 'https://www.jcrew.com/s7-img-facade/L2241_BL8133?$pdp_enlarge$',
+    pictureURL:
+      "https://www.jcrew.com/s7-img-facade/L2241_BL8133?$pdp_enlarge$",
     rating: 0,
     quantity: 1000,
   },
   {
-    title: 'Fedora',
-    description: 'The perfect accessory to business attire',
+    title: "Fedora",
+    description: "The perfect accessory to business attire",
     price: 3000,
-    pictureURL: 'https://cdn.shopify.com/s/files/1/2121/1571/products/STW308_GREY_3Q_1000x.jpg?v=1612373505',
+    pictureURL:
+      "https://cdn.shopify.com/s/files/1/2121/1571/products/STW308_GREY_3Q_1000x.jpg?v=1612373505",
     rating: 0,
     quantity: 800,
   },
   {
-    title: 'Sun hat',
-    description: 'Stay made in the shade with this stylish head piece',
+    title: "Sun hat",
+    description: "Stay made in the shade with this stylish head piece",
     price: 1500,
-    pictureURL: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR3Hgl9DWse7J0k3V_grMbdcSCinz2UFhijtR2tWjyy9h5UwhvY3rdEzi1ENCR3Ybv75tb4RqmusVH8p-ef83PwJ0MKlYavjjtGGftw-b8pmJsW7bnKKWzlgQ&usqp=CAE',
+    pictureURL:
+      "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR3Hgl9DWse7J0k3V_grMbdcSCinz2UFhijtR2tWjyy9h5UwhvY3rdEzi1ENCR3Ybv75tb4RqmusVH8p-ef83PwJ0MKlYavjjtGGftw-b8pmJsW7bnKKWzlgQ&usqp=CAE",
     rating: 0,
     quantity: 1250,
   },
   {
-    title: 'Beret',
-    description: 'Rapsberry beret, the kind you find in a scecond hand store',
+    title: "Beret",
+    description: "Rapsberry beret, the kind you find in a scecond hand store",
     price: 2000,
-    pictureURL: 'https://images-na.ssl-images-amazon.com/images/I/41Cqe9LdgcL._AC_.jpg',
+    pictureURL:
+      "https://images-na.ssl-images-amazon.com/images/I/41Cqe9LdgcL._AC_.jpg",
     rating: 0,
     quantity: 1500,
   },
   {
-    title: 'Cowboy',
-    description: 'Howdy like this ten gallon partner!',
+    title: "Cowboy",
+    description: "Howdy like this ten gallon partner!",
     price: 5000,
-    pictureURL: 'https://images-na.ssl-images-amazon.com/images/I/61BBPX8VzYL._AC_UX569_.jpg',
+    pictureURL:
+      "https://images-na.ssl-images-amazon.com/images/I/61BBPX8VzYL._AC_UX569_.jpg",
     rating: 0,
     quantity: 1000,
   },
-]
+];
 
 async function seed() {
-  await db.sync() // clears db and matches models to tables
-  console.log('db synced!')
+  await db.sync({ force: true }); // clears db and matches models to tables
+  console.log("db synced!");
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123', email: 'example@gmail.com' }),
-    User.create({ username: 'murphy', password: '123', email: 'murphy@gmail.com' }),
-  ])
+    User.create({
+      // username: "cody",
+      password: "123",
+      email: "example@gmail.com",
+    }),
+    User.create({
+      // username: "murphy",
+      password: "123",
+      email: "murphy@gmail.com",
+    }),
+  ]);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded successfully`);
 
-  const hats = await Product.bulkCreate(products)
+  const hats = await Product.bulkCreate(products);
 
-  console.log(`seeded ${hats.length} products`)
-  console.log(`seeded successfully`)
+  console.log(`seeded ${hats.length} products`);
+  console.log(`seeded successfully`);
 
   return {
     users: {
       cody: users[0],
-      murphy: users[1]
-    }
-  }
+      murphy: users[1],
+    },
+  };
 }
 
 /*
@@ -91,16 +105,16 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -110,8 +124,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
