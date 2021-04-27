d23236d64e6251a3eb401f48cd53e122a0fffb9a
Gezahegn Starr <starrgez@msu.edu>
2021-04-27 18:57:59 -0400

    FE: fixed highlight-navbar-css

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,10 +7,19 @@ import history from '../history';
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      location: history.location.pathname
+    }
+  }
+  handleClick(event){
+    const curLocation = history.location.pathname.split('/')[1]
+    this.setState({location:event.target.name})
   }
   render() {
-    const { handleClick, isLoggedIn } = this.props;
-    //let currentUrl = this.props.location.split('/')[1] || '';
+    const { handleClick:logOut, isLoggedIn } = this.props;
+    const location = history.location.pathname
+    let homeCondition = '/home' === location || '/products' === location
     return (
       <div>
         <h1>Grace Toppers</h1>
@@ -19,25 +28,49 @@ class Navbar extends React.Component {
             <div>
               {/* The navbar will show these links after you log in */}
               <Link
+                name='/home'
+                onClick={this.handleClick}
                 to="/home"
-                className={'products' === currentUrl ? 'selected' : ''}
+                className={ homeCondition ? 'selected' : ''}
               >
                 Home
               </Link>
-              <Link to="/cart">Cart</Link>
-              <Link to="/orders">Orders</Link>
+              <Link
+              to="/cart"
+              name='/cart'
+              onClick={this.handleClick}
+              className={'/cart' === location ? 'selected' : ''}
+              >Cart</Link>
+
+              <Link
+              to="/orders"
+              name='/orders'
+              onClick={this.handleClick}
+              className={'/orders' === location ? 'selected' : ''}
+              >Orders</Link>
               {/* <Link to="/account">Account Info</Link> */}
-              <a href="#" onClick={handleClick}>
+              <a href="#"
+              onClick={logOut}
+              >
                 Logout
               </a>
             </div>
           ) : (
             <div>
               {/* The navbar will show these links before you log in */}
-              <Link to="/home">Home</Link>
-              <Link to="/cart">Cart</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
+              <Link to="/home" onClick={this.handleClick} name='/home'
+              className={homeCondition? 'selected' : ''}
+              >Home</Link>
+
+              <Link to="/cart" onClick={this.handleClick} name='/cart'
+              className={'/cart' === location ? 'selected' : ''}
+              >Cart</Link>
+              <Link to="/login" onClick={this.handleClick} name='/login'
+              className={'/login' === location ? 'selected' : ''}
+              >Login</Link>
+              <Link to="/signup" onClick={this.handleClick} name='/signup'
+              className={'/signup' === location ? 'selected' : ''}
+              >Sign Up</Link>
             </div>
           )}
         </nav>
